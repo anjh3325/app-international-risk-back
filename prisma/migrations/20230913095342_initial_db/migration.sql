26304ca292718b8466bc24f625f441950d8ee54a
Anjh <anjh3325@gmail.com>
2023-09-13 18:54:44 +0900

    prisma연결

---
+++ prisma/migrations/20230913095342_initial_db/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `country` VARCHAR(191) NOT NULL,
+    `writer` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_writer_fkey` FOREIGN KEY (`writer`) REFERENCES `User`(`name`) ON DELETE RESTRICT ON UPDATE CASCADE;
